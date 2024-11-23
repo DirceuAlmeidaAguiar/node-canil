822dc2591cee97dda73d067dd06e70dc208ae821
Dirceu <dirceuaguiar1963@gmail.com>
2024-11-23 14:33:54 -0300

    final do projeto

--- src/controllers/pagerController.ts
+++ src/controllers/pagerController.ts
@@ -1,16 +1,54 @@
 import { Request, Response } from "express";
+import { createMenuObeject } from "../helpers/createMenuObject";
+import { Pet } from "../models/pet";
 
 export const home = (req: Request, res: Response)=>{
-    res.send('Início da página funcionando!')
-    // res.render('pages/page')
+    let list = Pet.getAll()
+
+    res.render('pages/page', {
+        menu: createMenuObeject('all'),
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        },
+        list
+    })
 }
 export const dogs = (req: Request, res: Response)=>{
-    // res.render('pages/page')
+
+    let list = Pet.getFromType('dog')
+    res.render('pages/page', {
+        
+        menu: createMenuObeject('dog'),
+            banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        },
+        list
+    })
 }
 export const cats = (req: Request, res: Response)=>{
-    // res.render('pages/page')
+    let list = Pet.getFromType('cat')
+    res.render('pages/page', {
+        menu: createMenuObeject('cat'),
+        
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        }, 
+        list
+    })
 }
 export const fishes = (req: Request, res: Response)=>{
-    // res.render('pages/page')
+    let list = Pet .getFromType('fish')
+    res.render('pages/page', {
+        menu: createMenuObeject('fish'),
+        
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        },
+        list
+    })
 }
 
--- src/server.ts
+++ src/server.ts
@@ -12,14 +12,14 @@ server.set('view engine', 'mustache')
 server.set('views', path.join(__dirname, 'views'))
 server.engine('mustache', mustache())
 
-server.use(express.static(path.join(__dirname, '../src/public')))
+server.use(express.static(path.join(__dirname, '../public')))
 
 // Aqui ficarão as rotas
 
 server.use(mainRoutes)
 
 server.use((req, resp)=>{
-    resp.send('Página não foi encontrada. Tente Novamente...')
+    resp.render('pages/404')
 })
 
 server.listen(process.env.PORT)
